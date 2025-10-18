5a713b0cfbc2f7c0a870123db3f15f641c3f9d9e
TBPanha11 <novpanha@gmail.com>
2025-10-19 01:07:38 +0700

    Update IndexPage.vue to change staging page text to production page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,13 @@
 <template>
   <q-page class="flex flex-center">
-    <div>Staging Page</div>
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    />
+    <div>
+      Production Page
+      <img
+        alt="Quasar logo"
+        src="~assets/quasar-logo-vertical.svg"
+        style="width: 200px; height: 200px"
+      />
+    </div>
   </q-page>
 </template>
 
